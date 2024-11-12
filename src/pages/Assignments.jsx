d31a2234048a39c52720fc64492ce7c4cfd4e1a9
Maja0306 <Majand01@iba.dk>
2024-11-12 13:02:25 +0100

    Lidt rettelser i css og tilføjelse flere karaktere

--- src/pages/Assignments.jsx
+++ src/pages/Assignments.jsx
@@ -1,10 +1,14 @@
 import Navbar from "../allComponents/Navbar"
+import AddTask from "../assignments/AddTask"
+import Filter from "../assignments/Filter"
 
 function Assignments() {
 
   return (
     <>
       <h1>Assignments</h1>
+      <AddTask />
+      <Filter />
       <Navbar />
     </>
   )
