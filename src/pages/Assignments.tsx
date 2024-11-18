aaaf5b2938e4271dfa190036961880221b9f7298
Maja0306 <Majand01@iba.dk>
2024-11-18 22:03:37 +0100

    Tilføjet context i assignments

--- src/pages/Assignments.tsx
+++ src/pages/Assignments.tsx
@@ -2,11 +2,12 @@ import Navbar from "../allComponents/Navbar";
 import AddTask from "../assignments/AddTask";
 import Filter from "../assignments/Filter";
 import React, { useContext } from 'react';
-import { TaskContext } from "../context/TaskContext"; 
 import { Link } from "react-router-dom";
+import { CharacterContext } from "../context/CharacterContext"; 
 
 function Assignments() {
-  
+  const { tasks, setTasks} = useContext(CharacterContext);
+
   return (
     <>
       <h1>Mine opgaver</h1>
