c3d3a0cfe81c80d18d9e48cdf50ad07f1c736561
Vevest <vvevest@gmail.com>
2024-11-18 15:36:38 +0100

    filter slettet udkom.

--- src/assignments/Filter.tsx
+++ src/assignments/Filter.tsx
@@ -2,46 +2,6 @@ import { useState } from 'react';
 import React, { useContext } from "react";
 import { TaskContext } from "../context/TaskContext";
 
-// // Chores by category
-// const booksChores = [
-//   "Read a book", "Shower", "Exercise", "Meditate"
-// ];
-
-// const kitchenChores = [
-//   "Vacuum", "Do the dishes", "Make the bed", "Clean the bathroom",
-//   "Take the trash out", "Clean the kitchen", "Water indoor plants"
-// ];
-// const washChores = [
-//   "Clean the windows", "Mow the lawn", "Water outdoor plants",
-//   "Tend the garden", "Clean the car"
-// ];
-// const clothesChores = [
-//   "Do the laundry", "Hang up the laundry", "Fold the laundry",
-//   "Put laundry in closet"
-// ];
-
-// const bedroomChores = [
-//   ""
-// ];
-
-// const livingroomChores = [
-//   ""
-// ];
-
-// const bagsChores = [
-//   ""
-// ];
-
-// // Combine categories into a single array
-// const choresData = [
-//   ...booksChores.map((name, index) => ({ id: `books-${index}`, name, category: "Books" })),
-//   ...kitchenChores.map((name, index) => ({ id: `kitchen-${index}`, name, category: "Kitchen" })),
-//   ...washChores.map((name, index) => ({ id: `wash-${index}`, name, category: "Wash" })),
-//   ...clothesChores.map((name, index) => ({ id: `clothes-${index}`, name, category: "Clothes" })),
-//   ...livingroomChores.map((name, index) => ({ id: `livingroom-${index}`, name, category: "Livingroom" })),
-//   ...bedroomChores.map((name, index) => ({ id: `bedroom-${index}`, name, category: "Bedroom" })),
-//   ...bagsChores.map((name, index) => ({ id: `bags-${index}`, name, category: "Bags" })),
-// ];
 
 // Define emojis for each category
 const categoryEmojis = [
