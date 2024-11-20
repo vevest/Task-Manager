24c1ebde971b0d67469ac725c426b7e371f8b301
Vevest <vvevest@gmail.com>
2024-11-20 14:37:24 +0100

    filer ændret context og last done

--- src/allComponents/LastDone.tsx
+++ src/allComponents/LastDone.tsx
@@ -1,10 +1,27 @@
+import { useContext } from "react";
+import { CharacterContext } from "../context/CharacterContext";
+
 function LastDone() {
-  return(
-    <>
-      <p>Sidst gjort</p>
-      
-    </>
-  )
+  const { completedTasks, tasks } = useContext(CharacterContext);
+
+  return (
+    <div className="last-done-container">
+      <h2>Sidst Gjorte Opgaver</h2>
+      <ul>
+        {completedTasks.length === 0 ? (
+          <p>Ingen opgaver er færdiggjort endnu.</p>
+        ) : (
+          completedTasks.map((task) => (
+            <li key={task.id}>
+              <p>
+                {task.name} - {task.category} - {task.points}⚡️
+              </p>
+            </li>
+          ))
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export default LastDone;
